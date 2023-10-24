d3e7addbabfaad6ac55d3a4f96ffeb58b26fdf5c
Noah <noahzune@live.com>
2023-10-24 11:23:19 -0700

    404 page and removed old /users

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var searchRouter = require('./routes/tractorSearch');
 var detailsRouter = require('./routes/tractorDetails');
 
@@ -26,28 +25,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
 
-// const bodyParser = require('body-parser');
-// app.use(bodyParser);
-
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/tractorSearch', searchRouter);
 app.use('/tractorDetails', detailsRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use( ( req, res, next ) => {
+  res.status( 404 ).sendFile( __dirname + '/public/404.html' );
 });
 
 module.exports = app;
