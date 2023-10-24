81e20b404ea59137c1a524234747fd82b4c84c2e
Noah N <noahzune@live.com>
2023-10-24 09:47:43 -0700

    text update + 'latest' page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var searchRouter = require('./routes/tractorSearch');
 var detailsRouter = require('./routes/tractorDetails');
+var latestRouter = require('./routes/latest');
 
 
 
@@ -33,6 +34,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tractorSearch', searchRouter);
 app.use('/tractorDetails', detailsRouter);
+app.use('/latest', latestRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
